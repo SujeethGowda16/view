dbbe686596b238fc6f2147d860171ace94e572e4
SujeethGowda16 <81603492+SujeethGowda16@users.noreply.github.com>
2021-03-31 20:21:32 +0530

    Update service.service.ts

--- service.service.ts
+++ service.service.ts
@@ -9,14 +9,6 @@ export class ServiceService {
        product:object;
   constructor(private hc:HttpClient) { }
 
-    addUser(userObj:object):Observable<any>{
-      return this.hc.post("/user/register",userObj);
-    }
-
-   loginUser(userObj:object):Observable<any>{
-      return this.hc.post("/user/login",userObj)
-    }
-
     addProduct(userObj:object):Observable<any>{
       return this.hc.post("/product/addproduct",userObj)
     }
@@ -29,40 +21,5 @@ export class ServiceService {
    editProduct(obj:object):Observable<any>{
      return this.hc.put("/product/updateproduct",obj)
     }
-
-    deleteProduct(obj:object):Observable<any>{
-      console.log("from delete product",obj)
-      return this.hc.post("/product/removeproduct",obj)
-    }
-    
-    // Carousel
-    uploadCarousel(obj:object):Observable<any>{
-      return this.hc.post("/admin/addcarousel",obj)
-    }
-   
-    getcarousel():Observable<any>{
-      return this.hc.get("/admin/def")
-    }
-
-    editcarousel(obj:object):Observable<any>{
-      return this.hc.put("/admin/updatecarousel",obj)
-     }
- 
-     deletecarousel(obj:object):Observable<any>{
-       console.log("from delete product",obj)
-       return this.hc.post("/admin/removecarousel",obj)
-     }
-
-      getOneProduct(productname:string){
-        return this.hc.get("/product/oneproduct/"+productname)
-      }
-
-      addCart(product:object):Observable<any>{
-        return this.hc.post("/cart/addtocart",product)
-      }
-
-      getCart(username:string):Observable<any>{
-         return this.hc.get("/cart/getcart/"+username)
-      }
-     
+  
 }
